f0498a9da3314eb624d7ddc473039890ffb0b412
Alysson Oliveira <3oalysson@gmail.com>
2022-07-05 19:12:53 -0300

    Ajuste de projeto

---
+++ OO Project/UNO/UNO.BL/Customer.cs
@@ -0,0 +1,59 @@
+﻿
+namespace UNO.BL
+
+{
+    public class Customer
+    {
+        public Customer()
+        {
+                       
+        }
+
+        public Customer(int customerId)
+        {
+            CustomerId = customerId;
+        }
+
+        public int CustomerId { get; private set; }
+
+        public string EmailAddres { get; set; }
+        public string FirstName { get; set; }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string FullName
+        {
+
+            get
+            {
+                string fullname = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullname))
+                    {
+                        fullname += ", ";
+                    }
+                    fullname += FirstName;
+                }
+                return fullname;
+            }
+
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddres)) isValid = false;
+
+            return isValid;
+
+        }
+    }
+}
---
+++ OO Project/UNO/UNO.BLTest/ProductTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UNO.BL;
+
+
+namespace UNO.BLTest
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void ProductValidation()
+        {
+            Product product = new Product(1);
+
+            product.ProductName = "Meia cano curto";
+            product.ProductDescription = "Usar no pé";
+            product.CurrentPrice = 30;
+
+
+            bool expected = true;
+
+            bool actual = product.Validate();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
